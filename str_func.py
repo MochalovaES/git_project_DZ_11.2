fa608e91769386630219aa69b65d5b25a19fc10b
Mochalova ES <mochalova.es@mail.ru>
2023-08-06 21:27:30 +0300

    add new functions

--- str_func.py
+++ str_func.py
@@ -1,3 +1,14 @@
+'''
+Функция делает буквы строки заглавными
+'''
 def str_func(string):
     string_upper = string.upper()
     return string_upper
+
+
+'''
+Функция делает заглавными первые буквы каждого слова в строке
+'''
+def string_func(string):
+    string_capitalize = string.capitalize()
+    return string_capitalize
